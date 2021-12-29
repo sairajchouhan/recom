e1fc20f43cb58699d6859885d6aab6927ef53332
sairaj <sairaj2119@gmail.com>
2021-12-29 12:37:00 +0530

    seed products to database

---
+++ prisma/migrations/20211229063903_add_color/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "color" TEXT,
+ADD COLUMN     "url" TEXT;
--- prisma/script.ts
+++ prisma/script.ts
@@ -1,14 +1,61 @@
-import { db } from '~/utils/db.server'
 import products from './data.json'
+import { PrismaClient } from '@prisma/client'
 
-const add_products = () => {
+const db = new PrismaClient({
+  datasources: {
+    db: {
+      url: 'postgresql://postgres:password@localhost:5432/recom?schema=public',
+    },
+  },
+})
+
+const add_products = async () => {
+  const men_tshirts_category_ids = (await db.category.findMany({})).map(
+    (item) => item.id
+  )
+
+  const promises: Array<Promise<any>> = []
+
   products.forEach((product) => {
-    db.product.create({
-      data: {
-        name: product.name,
-        description: product.description,
-        price: product.price,
-      },
-    })
+    promises.push(
+      db.product.create({
+        data: {
+          name: product.name,
+          description: product.description,
+          price: product.price,
+          color: product.color,
+          url: product.url,
+          category: {
+            connect: men_tshirts_category_ids.map((item) => ({ id: item })),
+          },
+        },
+      })
+    )
+  })
+  await Promise.all(promises)
+}
+
+const add_categories = async () => {
+  const categories = [
+    {
+      name: 'men',
+      description: 'for men',
+    },
+    {
+      name: 'tshirt',
+      description: 'tshirt',
+    },
+  ]
+
+  await db.category.createMany({
+    data: categories,
   })
 }
+
+add_products()
+  .then(() => {
+    console.log('✅')
+  })
+  .catch((err) => {
+    console.error(err)
+  })
